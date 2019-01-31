230c9da42160d83a2014a9896c53e210ecc1db49
Eduardo Fernandes Marques <onix978@gmail.com>
2019-01-30 23:16:30 -0200

    Simple e2e.

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,16 +1,16 @@
 import { AppPage } from './app.po';
 import { browser, logging } from 'protractor';
 
-describe('workspace-project App', () => {
+describe('dragons-project App', () => {
   let page: AppPage;
 
   beforeEach(() => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('Get developer name', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to dragons-project!');
+    expect(page.getDeveloperName()).toEqual('Criado por Eduardo Fernandes Marques');
   });
 
   afterEach(async () => {
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -5,7 +5,7 @@ export class AppPage {
     return browser.get('/') as Promise<any>;
   }
 
-  getTitleText() {
-    return element(by.css('app-root h1')).getText() as Promise<string>;
+  getDeveloperName() {
+    return element(by.css('p.footer')).getText() as Promise<string>;
   }
 }
